14139afcc34de3ecf0758d14f73605dc6291689c
andrewhenderson <andrew.m.henderson@gmail.com>
2015-09-12 15:55:15 -0700

    First docs example test working

--- test/unit/chain-router.js
+++ test/unit/chain-router.js
@@ -3,43 +3,35 @@ define(function(require){
   var Backbone = require('backbone');
   var ChainRouter = require('dist/backbone.chain-router');
 
-  describe('Chain Router', function(){
+  describe('Router,', function(){
 
-    describe('when routing to a matched route with chaining', function(){
+    describe('when executing a chained route,', function(){
 
       before(function(){
         var Router = Backbone.Router.extend({
           routes: {
-            'post/:id': 'posts.post'
+            'posts/new': 'posts.new'
           },
           posts: sinon.spy(),
-          post: sinon.spy()
+          new: sinon.spy()
         });
         this.router = new Router();
         Backbone.history.start();
       });
 
-      it('should call both routes in order', function(){
-        this.router.navigate('post/15',{trigger: true});
+      it('should call both routes', function(){
+        this.router.navigate('posts/new',{trigger: true});
         expect(this.router.posts).to.have.been.calledOnce;
-        expect(this.router.post).to.have.been.calledOnce;
+        expect(this.router.new).to.have.been.calledOnce;
       });
 
       afterEach(function(){
-        location.hash = '';
+        this.router.navigate('',{trigger: true});
       });
 
       after(function(){
         Backbone.history.stop();
       });
-
-      //beforeEach(function() {
-      //  this.router.navigate('posts/15');
-      //});
-      //
-      //it('should trigger onNavigate', function () {
-      //  expect(this.router.composeNestedRoute).to.be.a('function');
-      //});
     });
   });
 });
